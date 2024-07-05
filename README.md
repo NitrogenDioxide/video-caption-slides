d3b24e70b6ae82d4f3d3b4f3bcfdd954883bbd8f
Nitrogen Dioxide <skylineblueblueblue+centropy@gmail.com>
2024-07-04 22:12:38 -0400

    Scripts to generate Quarto file

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # video-caption-slides
 Some snippets to use Quarto to create a slide deck for visualizing videos
+
+## Usage 
+
+1. Update `video_data.jsonl` with the path to videos and the corresponding captsions.
+2. Execute `python generate_qmd.py`
+3. Then run `quarto render slides.qmd`
+
+To check the generated webpage, open `slides.html` in a browser.
---
+++ generate_qmd.py
@@ -0,0 +1,57 @@
+import json
+
+import typer
+
+HEADER_TEMPLATE = """---
+title: {title}
+format:
+  revealjs:
+    transition: slide
+    theme: simple
+    css: custom.css
+    background-transition: fade
+    transition-speed: fast
+    smaller: true
+    controls: true
+    slide-number: true
+    loop: true
+---
+"""
+
+
+SECTION_TEMPLATE = """
+##  {{data-menu-title="{data_menu_title}"}}
+
+<video data-autoplay loop src="{video_path}"></video>
+
+::: {{.scroll-container}}
+
+{caption}
+
+:::"""
+
+
+def main(
+    slide_title: str = "Example Video Captions",
+    video_data_file: str = "video_data.jsonl",
+    output_file: str = "slides.qmd",
+):
+    with open(video_data_file, "r") as f:
+        video_data = [json.loads(line) for line in f]
+
+    with open(output_file, "w") as f:
+        print(HEADER_TEMPLATE.format(title=slide_title), file=f)
+
+        for video in video_data:
+            print(
+                SECTION_TEMPLATE.format(
+                    data_menu_title=video["file"].split("/")[-1],
+                    video_path=video["file"],
+                    caption=video["caption"],
+                ),
+                file=f,
+            )
+
+
+if __name__ == "__main__":
+    typer.run(main)
